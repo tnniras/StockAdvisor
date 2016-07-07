81a9d306939239a458a0aa3b4634c99de7b3efef
Tushar Niras <tnniras@gmail.com>
2016-07-07 20:52:47 +0000

    Extra code for Symbol

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
 
+import yahoo_finance
 from yahoo_finance import Share
 
 # List of stocks
-stocks = [ "AMZN", "IBM", "YHOO"]
+stocks = [ "AMZN", "IBM", "RELIANCE"]
 
-for stock in stocks:
-    symbol = Share(stock)
-    print "%s : %s " % (stock, symbol.get_price())
 
-# COMMENT
-print 'Hello, world!'
+
+#def get_stock_price(stocks):
+ #   for stock in stocks:
+ #       symbol = Share(stock)
+ #       print "%s : %s " % (stock, symbol.get_price())
+
+#get_stock_price(stocks)
+
+tt = Share("BHEL.NS")
+#print yahoo_finance.__dict__
+print(tt.get_price())
---
+++ yahoo.py
@@ -0,0 +1,5 @@
+
+
+import financeAPI
+# get summery for today
+financeAPI.get_summery('ICICIBANK')
