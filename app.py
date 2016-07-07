8269172d7de41c060701bebbd7b60761f9584a2f
Tushar Niras <tnniras@gmail.com>
2016-07-07 12:07:53 +0000

    Get price from multiple stocks

--- app.py
+++ app.py
@@ -2,9 +2,15 @@
 
 from yahoo_finance import Share
 
-yahoo = Share('AMZN')
-print yahoo.get_price()
-print yahoo.get_trade_datetime()
+# List of stocks
+stocks = [ "AMZN", "IBM", "YHOO"]
+
+for stock in stocks:
+    symbol = Share(stock)
+    print "%s : %s " % (stock, symbol.get_price())
+
+#print yahoo.get_price()
+#print yahoo.get_trade_datetime()
 
 # COMMENT
 print 'Hello, world!'
