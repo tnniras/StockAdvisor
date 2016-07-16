ea7d936e8ee581211148c4ff869b8aac039a796a
Tushar Niras <tnniras@gmail.com>
2016-07-16 09:59:30 +0000

    Managed Directory structure and removed unwanted files, configured setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 version = '0.0.1'
 
 setup(
-    name='FinanceAPI',
+    name='StockAdvisor',
     version=version,
-    description='Script to extract data against Yahoo! Finance India',
-    author='Nar Kumar Chhantyal',
-    author_email='neokya@gmail.com',
-    url='https://github.com/neokya/FinanceAPI',
+    description='This application is used to get latest Stock price from the RealTime APIs',
+    author='Tushar Niras, ',
+    author_email='tnniras@gmail.com',
+    url='https://github.com/tnniras/StockAdvisor',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
